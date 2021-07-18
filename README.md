1a1838e1cb0c88b231e43b440e2ad9bd59998a38
Ahmad Fajar <87592519+Ahmadjarr@users.noreply.github.com>
2021-07-18 12:41:06 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Aplikasi-Stok-Barang
-ini adalah aplikasi web yang di buat untuk memudahkan gudang/tempat penyimpanan barang dalam mengelola barang yang masuk maupun keluar dan stock barang yang tersedia
+# Aplikasi-Stock-Barang-Web
+Apliaksi berbasis web yang dibuat untuk memudahkan gudang penyimpanan ataupun toko unutk mengetahui barang masuk,keluar dan stock barang yang tersedia
---
+++ index.php
@@ -0,0 +1,184 @@
+<?php 
+ require 'function.php';
+ require 'cek.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="https://fti.uad.ac.id/safety-laboratory-dan-pengenalan-alat-laboratorium-teknik-kimia/">
+        <img src="logouad.png" width="90px">Lab Teknik Informatika</a>
+        <a class="navbar-brand ps-3" ></a>
+            <a class="navbar-brand ps-3" href="masuk.php">Barang Masuk </a>
+            <a class="navbar-brand ps-3" href="keluar.php">Barang keluar</a>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+            <!-- Navbar-->
+        </nav>
+
+
+            <!-- new side bar -->
+             <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Stock Barang
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Setting
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="logout.php">Logout</a>
+
+                                </nav>
+                            </div>
+
+                            </div>
+
+                </nav>
+            </div>
+
+            <!-- close new sidebar -->
+            <div id="layoutSidenav_content">
+                <main>
+                    <br>
+                    <br>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">StockBarang</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">StockBarang</li>
+                        </ol>
+                        
+                        <div class="card mb-4">
+                            <div class="card-header">
+                        <!-- Button to Open the Modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                            Tambah Barang
+                        </button>
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Barang </th>
+                                            <th>Deskripsi </th>
+                                            <th>Stok</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                            <?php
+                            $ambilsemuadatastock = mysqli_query($conn,"select * from stock ");
+                            while($data= mysqli_fetch_array(($ambilsemuadatastock))){
+                                    $i=1;
+                                    $namabarang = $data['namabarang'];
+                                    $deskripsi = $data['deskripsi'];
+                                    $stock = $data['stock'];
+                                            ?>
+
+                                        <tr>
+                                            <td><?=$i++;?></td>
+                                            <td><?=$namabarang;?></td>
+                                            <td><?=$deskripsi;?></td>
+                                            <td><?=$stock;?></td>
+                                        </tr>
+                                        <?php
+                                    };
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; 16 Agustus 2021</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal" >
+    <div class="modal-dialog">
+    <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+        <h4 class="modal-title">Tambah Barang</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <form method="post">
+        <div class="modal-body">
+            <label>Nama Barang </label><br>
+        <input type="text" name="namabarang"  class="from-control"><br>
+        <br>
+            <label>Deskripsi</label><br>
+        <input type="text" name="deskripsi"  class="from-control"><br>
+        <br>
+            <label>Stock</label><br>
+        <input type="number" name="stock" class="from-control"><br>
+        <br>
+        <button type="submit" class="btn btn-primary" name="addnewbarang">Submit</button>
+        </div>
+        
+        </form>
+        <!-- Modal footer -->
+        <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+    </div>
+    </div>
+</div>
+
+</html>
